4f99ed487ad5b8627ec1e83a86f585f10110f8dc
github <github@zzxyz.no>
2024-10-16 12:48:11 +0200

    Several changes; see comments.
    - Changed import method.
    - Changed variables.
    - Removed superfluous `networking` defaults.
    - Corrected paths to new ones.

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -1,71 +1,71 @@
 # Host-specific system configuration defaults
-{ config, lib, pkgs, ... }:
-let
-  vars = import ../../vars/vars.nix;
-in
+{ config, lib, pkgs, myVars, ... }:
+
 {
   imports = [
     # Include the results of the hardware scan
     ./hardware-configuration.nix
 
-    # Testing
-    #../../modules/networking/systemd/defaults.nix
-    ../../modules/services/utilities/searx.nix # Approved
-    ../../modules/virtualization/vm/vm.nix
-
     # Boot configuration
-    ../../modules/boot/defaults.nix
-    ../../modules/boot/filesystem/defaults.nix
+    ../../modules/shared/boot/defaults.nix
+    ../../modules/shared/boot/filesystem/defaults.nix
+    ../../modules/server/boot/filesystem/filesystem.nix # Server-specific
 
-    # Environment configuration
-    ../../modules/environment/defaults.nix
+    # Environment
+    ../../modules/shared/environment/defaults.nix
 
-    # Locale configuration
-    ../../modules/i18n/defaults.nix
+    # Localization
+    ../../modules/shared/i18n/defaults.nix
 
-    # Network configuration
-    ../../modules/networking/defaults.nix
-    ../../modules/networking/ssh/ssh.nix
-    ../../modules/networking/tailscale/tailscale.nix
-    ../../modules/networking/vlan/vlan.nix
+    # Networking
+    ../../modules/shared/networking/defaults.nix
+    ../../modules/shared/networking/ssh/defaults.nix
+    ../../modules/shared/networking/systemd/defaults.nix
+    ../../modules/shared/networking/tailscale/defaults.nix
+    ../../modules/server/networking/systemd/systemd-networking.nix # Server-specific
+    ../../modules/server/networking/tailscale/tailscale.nix # Server-specific
+    ../../modules/server/networking/vlan/vlans.nix # Server-specific
 
-    # Nix configuration
-    ../../modules/nix/defaults.nix
+    # Packages
+    ../../modules/server/packages/system-packages.nix # Server-specific
 
-    # Package configuration
-    ../../modules/packages/default-packages.nix
+    # System
+    ../../modules/shared/nix/defaults.nix
 
-    # Programs configuration
-    ../../modules/programs/defaults.nix
+    # Programs
+    ../../modules/shared/programs/defaults.nix
 
-    # Security configuration
-    ../../modules/security/secrets/agenix.nix
-    ../../modules/security/secureboot/lanzaboote.nix
-    ../../modules/security/defaults.nix
+    # Security
+    ../../modules/shared/security/defaults.nix
+    ../../modules/shared/security/secrets/agenix.nix
+    ../../modules/shared/security/secureboot/lanzaboote.nix
 
-    # Services configuration
-    ../../modules/services/media/plex.nix
-    ../../modules/services/utilities/atuin.nix
-    ../../modules/services/utilities/cockpit.nix
-    ../../modules/services/utilities/printing.nix
-    ../../modules/services/utilities/scrutiny.nix
+    # Services
+    ../../modules/shared/services/utilities/atuin.nix
+    ../../modules/shared/services/utilities/printing.nix
+    ../../modules/server/services/media/plex.nix # Server-specific
+    ../../modules/server/services/utilities/cockpit.nix # Server-specific
+    #../../modules/server/services/utilities/firefly.nix # Server-specific; not created yet
+    ../../modules/server/services/utilities/scrutiny.nix # Server-specific
+    ../../modules/server/services/utilities/searx.nix # Server-specific
 
-    # Virtualization configuration
-    ../../modules/virtualization/containers/podman.nix
-    ../../modules/virtualization/orchestration/k3s.nix
-    ../../modules/virtualization/vm/microvm.nix
+    # Virtualization
+    ../../modules/shared/virtualization/containers/docker.nix
+    ../../modules/shared/virtualization/containers/podman.nix
+    ../../modules/shared/virtualization/vm/microvm.nix
+    ../../modules/shared/virtualization/vm/vm.nix
+    ../../modules/server/virtualization/containers/docker.nix # Server-specific
+    ../../modules/server/virtualization/containers/podman.nix # Server-specific
+    ../../modules/server/virtualization/orchestration/k3s.nix # Server-specific
 
-    # User configuration
+    # Users
     ../../users/server.nix
   ];
 
   networking = {
-    hostName = "${vars.hostname}";
-    hostId = "${vars.hostId}";
+    hostName = myVars.server.hostname;
+    hostId = myVars.server.hostId;
 
-    useNetworkd = true;
-    useDHCP = false; # Defaults to true; disabled for systemd-networkd
-    networkmanager.enable = false;
     wireless.enable = false;
   };
 
