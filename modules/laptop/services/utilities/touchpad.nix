f6a579a29c187dcd67d398cff5627d188d3a4056
github <github@zzxyz.no>
2024-10-17 12:09:42 +0200

    First commit.

---
+++ modules/laptop/services/utilities/touchpad.nix
@@ -0,0 +1,6 @@
+{ config, lib, pkgs, myVars, ... }:
+
+{
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+}
---
+++ users/main-user.nix
@@ -0,0 +1,19 @@
+# Host-specific system configuration defaults
+{ config, lib, pkgs, myVars, ... }:
+
+{
+  users.users.${myVars.laptop.user} = {
+    isNormalUser = true;
+    description = myVars.laptop.user;
+    extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
+
+    packages = with pkgs; [
+      # Your packages here
+    ];
+
+    openssh.authorizedKeys.keys = [
+      myVars.laptop.openSSHPubKey
+      myVars.laptop.openSSHGPGPubKey
+    ];
+  };
+}
