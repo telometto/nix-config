65b9df871128010c994f886a345a82c0e5d2002a
github <github@zzxyz.no>
2024-10-17 12:09:22 +0200

    First commit.

---
+++ modules/laptop/services/utilities/sound.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, myVars, ... }:
+
+{
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+}
