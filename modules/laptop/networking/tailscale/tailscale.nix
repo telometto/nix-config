e5ff00c00f3d6cd3e679fc2229ec8fb0f55a6fd6
github <github@zzxyz.no>
2024-10-17 12:09:09 +0200

    First commit.

---
+++ modules/laptop/networking/tailscale/tailscale.nix
@@ -0,0 +1,11 @@
+# Host-specific system configuration defaults
+{ config, lib, pkgs, ... }:
+
+{
+  services.tailscale = {
+    extraUpFlags = [
+      "--ssh"
+      "--accept-routes"
+    ];
+  };
+}
