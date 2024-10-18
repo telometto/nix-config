d65e7827c65e255b027fbc473f7ac75ccf490cee
github <github@zzxyz.no>
2024-10-18 22:12:33 +0200

    Enabled Pipewire sound.

--- modules/laptop/services/utilities/sound.nix
+++ modules/laptop/services/utilities/sound.nix
@@ -2,10 +2,16 @@
 
 {
   # Enable sound.
-  # hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false; # Default: true
   # OR
-  services.pipewire = {
+  services.pipewire = lib.mkIf (!hardware.pulseaudio.enable) {
     enable = true;
-    pulse.enable = true;
+    pulse = { enable = true; };
+    jack = { enable = false; };
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
   };
 }
