bdd41159738ba983ccb630f03639201d2721325e
github <github@zzxyz.no>
2024-10-19 15:53:42 +0200

    Added `dconf` settings; some do not work.

--- modules/laptop/home/home.nix
+++ modules/laptop/home/home.nix
@@ -32,6 +32,51 @@
     ];
   };
 
+  dconf = {
+    enable = true;
+
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        clock-show-weekday = true;
+        font-antialiasing = "rgba";
+        gtk-theme = "Adwaita-dark";
+      };
+
+      "org/gnome/desktop/datetime" = {
+        automatic-timezone = true;
+        automatic-timezone-guess = true;
+        automatic-timezone-guess-geoclue = true;
+      };
+
+      "org/gnome/desktop/peripherals/mouse" = {
+        #speed = "-0.5";
+        accel-profile = "flat";
+      };
+
+      "org/gtk/Settings/FileChooser" = {
+        sort-directories-first = true;
+      };
+
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = "appmenu:minimize,maximize,close";
+      };
+
+      "org/gnome/desktop/calendar" = {
+        show-weekdate = true;
+      };
+
+      "org/gnome/system/proxy" = {
+        mode = "auto";
+      };
+
+      "org/gnome/settings-daemon/plugins.color" = {
+        night-light-enabled = true;
+        night-light-schedule-automatic = true;
+      };
+    };
+  };
+
   programs = {
     home-manager = {
       enable = true;
