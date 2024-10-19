1942b8c1beb270930031cab6ca7365a17a7db942
github <github@zzxyz.no>
2024-10-19 14:09:23 +0200

    Removed `withJava` and `steam`; enabled `gamescope`.

--- modules/laptop/programs/steam.nix
+++ modules/laptop/programs/steam.nix
@@ -9,17 +9,18 @@
       enable = true;
       extest.enable = true; # Translate X11 input events to Wayland
       protontricks.enable = true; # Run Proton games with custom settings
+      gamescopeSession.enable = true; # Enable GameScope session
 
       extraPackages = with pkgs; [
         gamescope
         #protontricks
-        steam
+        #steam
         #steam-run
         steamcontroller
         steamtinkerlaunch
       ];
 
-      package = pkgs.steam.override { withJava = true; };
+      #package = pkgs.steam.override { withJava = true; }; # Deprecated since 23.05
     };
 
     java.enable = true;
