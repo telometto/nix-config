11445c83ca54dce57a06743fdaf790211a563c90
github <github@zzxyz.no>
2024-10-19 15:54:00 +0200

    Added extra user.

--- users/main-user.nix
+++ users/main-user.nix
@@ -2,18 +2,35 @@
 { config, lib, pkgs, myVars, ... }:
 
 {
-  users.users.${myVars.laptop.user} = {
-    isNormalUser = true;
-    description = myVars.laptop.description;
-    extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
+  users.users = {
+    ${myVars.laptop.user} = {
+      isNormalUser = true;
+      description = myVars.laptop.description;
+      extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
+  
+      packages = with pkgs; [
+        # Your packages here
+      ];
+  
+      openssh.authorizedKeys.keys = [
+        #myVars.laptop.openSSHPubKey
+        myVars.laptop.openSSHGPGPubKey
+      ];
+    };
 
-    packages = with pkgs; [
-      # Your packages here
-    ];
-
-    openssh.authorizedKeys.keys = [
-      #myVars.laptop.openSSHPubKey
-      myVars.laptop.openSSHGPGPubKey
-    ];
+    ${myVars.extraUsers.user} = {
+      isNormalUser = true;
+      description = myVars.extraUsers.description;
+      extraGroups = [ ];
+  
+      packages = with pkgs; [
+        # Your packages here
+      ];
+  
+      openssh.authorizedKeys.keys = [
+        #myVars.laptop.openSSHPubKey
+        myVars.laptop.openSSHGPGPubKey
+      ];
+    };
   };
 }
