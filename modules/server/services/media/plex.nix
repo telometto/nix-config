b703f3c9b27feb9b9970c1f033062461dec7d8cb
github <github@zzxyz.no>
2024-10-22 10:37:20 +0200

    Modified variable.

--- modules/server/services/media/plex.nix
+++ modules/server/services/media/plex.nix
@@ -6,7 +6,7 @@
     enable = true;
     openFirewall = true;
     user = myVars.mainUsers.server.user; # If not set, the service will run as user "plex"
-    dataDir = myVars.mainUsers.server.plexDataDir; # If not set, the service will use the default data directory
+    dataDir = myVars.server.plexDataDir; # If not set, the service will use the default data directory
   };
 
   environment.systemPackages = with pkgs; [ plex ];
