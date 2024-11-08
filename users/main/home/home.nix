c3218f3d93c3f735230af8b9d42d941224466dda
telometto <65364211+telometto@users.noreply.github.com>
2024-11-08 12:17:53 +0100

    Added imports and some packages; removed some packages.

--- users/main/home/home.nix
+++ users/main/home/home.nix
@@ -1,28 +1,46 @@
 { config, lib, pkgs, myVars, ... }:
 
 {
-  imports = [ ./programs.nix ]
-    ++ lib.optional myVars.general.enableGnome ./gnome.nix
-    ++ lib.optional myVars.general.enableKDE ./kde.nix;
+  imports = [
+    ./kde.nix
+
+    ./programs.nix
+    ./services.nix
+    ./xdg.nix
+  ];
+  # ++ lib.optional myVars.general.enableGnome ./gnome.nix
+  # ++ lib.optional myVars.general.enableKDE ./kde.nix;
 
   home = {
-    username = myVars.mainUsers.laptop.user;
+    username = myVars.mainUsers.desktop.user;
     stateVersion = "24.05";
 
     packages = with pkgs; [
-      # Your packages here
+      # Utils
       atuin
-      bash
-      bat
       #blesh
-      direnv
-      eza
-      #firefox
-      fzf
-      plasma5Packages.kdeconnect-kde
-      nix-direnv
-      sqlite
-      zoxide
+      #deja-dup
+      gparted
+      meld
+      polychromatic
+      variety
+
+      # Gaming
+      steam
+      #mangohud
+
+      # Media
+      texlivePackages.scheme-full
+      mpv
+
+      # Internet
+      brave
+      protonmail-desktop
+      thunderbird
+
+      # Social
+      discord
+      element-desktop
 
       # VS Code
       nixd # Nix language server for VS Code
