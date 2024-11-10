9035cb0105bfc6d39dba12c6787733fa381371b0
telometto <65364211+telometto@users.noreply.github.com>
2024-11-10 21:14:17 +0100

    Added (commented) code.

--- modules/shared/security/secrets/agenix.nix
+++ modules/shared/security/secrets/agenix.nix
@@ -3,5 +3,9 @@
 {
   imports = [ inputs.agenix.nixosModules.default ];
 
+  #age.identityPaths = [
+  #  "/home/${myVars.mainUsers.server.user}/secrets/mySecret.age"
+  #];
+
   environment.systemPackages = [ inputs.agenix.packages."${myVars.general.system}".default ];
 }
