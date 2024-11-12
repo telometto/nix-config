61cc463e53de5e9d4aee6fe89ab91be0a12282d1
telometto <65364211+telometto@users.noreply.github.com>
2024-11-12 23:32:00 +0100

    [TESTING] First commit.

---
+++ modules/services/documents/paperless.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, myVars, ... }:
+
+{
+  #environment.etc."paperlessFile".text = "admin";
+
+  services.paperless = {
+    enable = true;
+
+    address = "0.0.0.0"; # Defaults to localhost
+    # port = ; # Defaults to 28981
+
+    # openMPThreadingWorkaround = true; # Defaults to true
+    consumptionDirIsPublic = true; # Defaults to false
+
+    consumptionDir = "/tank/apps/nixos/paperless-ngx/consumption";
+    mediaDir = "/tank/apps/nixos/paperless-ngx/media";
+    dataDir = "/tank/apps/nixos/paperless-ngx/data";
+    passwordFile = myVars.paperless.passwordFile;
+
+    settings = {
+      PAPERLESS_OCR_LANGUAGE = "eng+nor+ita";
+      #PAPERLESS_OCR_LANGUAGES = "eng+nor+ita";
+
+      PAPERLESS_CONSUMER_IGNORE_PATTERN = builtins.toJSON [
+        ".DS_STORE/*"
+        "desktop.ini"
+      ];
+
+      PAPERLESS_OCR_USER_ARGS = builtins.toJSON {
+        optimize = 1;
+        pdfa_image_compression = "lossless";
+      };
+
+      PAPERLESS_TIKA_ENABLED = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ paperless-ngx ];
+}
