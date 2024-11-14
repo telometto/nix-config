3c7ff02012f2159458b397d911e2357fb897adce
telometto <65364211+telometto@users.noreply.github.com>
2024-11-14 09:46:01 +0100

    Added settings to `gpg-agent`; enabled `ssh-agent`.

--- common/users/server/home/services.nix
+++ common/users/server/home/services.nix
@@ -5,20 +5,25 @@
     gpg-agent = {
       enable = true;
 
-      enableSshSupport = true;
+      enableSshSupport = false;
       enableExtraSocket = true;
       enableBashIntegration = true;
       enableZshIntegration = true;
       enableScDaemon = false; # Smartcard
 
+      defaultCacheTtl = 34560000; # 400 days
+      defaultCacheTtlSsh = 34560000; # 400 days
+      maxCacheTtl = 34560000; # 400 days
+      maxCacheTtlSsh = 34560000; # 400 days
+
       sshKeys = [ "727A831B39D2FAC421617C2C203BF5C382E3B60A" ];
+
+      # extraConfig = ''
+      #   allow-preset-passphrase
+      # '';
     };
 
-    ssh-agent = {
-      enable = true;
-
-      addKeysToAgent = "yes";
-    };
+    ssh-agent = { enable = true; };
 
     #kdeconnect.enable = true; # Plasma 5
   };
