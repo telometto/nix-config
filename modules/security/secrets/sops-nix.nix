9f502fe0c1c108182de23fe19f99731d81c9f94c
telometto <65364211+telometto@users.noreply.github.com>
2024-11-21 16:02:15 +0100

    First commit.

--- modules/security/secrets/sops-nix.nix
+++ modules/security/secrets/sops-nix.nix
@@ -1,10 +1,44 @@
-{ config, inputs, lib, pkgs, myVars, ... }:
+{ config, inputs, lib, pkgs, ... }:
 
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
-  #sops = {
-  #  defaultSopsFile = "/home/${myVars.mainUsers.server.user}/.config/sops/gpg/secrets.yaml";
-  #  defaultSopsFormat = "yaml";
-  #};
+  sops = {
+    defaultSopsFile = ./secrets/secrets.yaml;
+    defaultSopsFormat = "yaml"; # Default format for sops files
+
+    age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ]; # Paths to the host ssh keys
+      keyFile = "/var/lib/sops-nix/key.txt"; # Path to the key file
+      generateKey = true; # Generate a new key if the keyFile does not exist
+    };
+
+    secrets = {
+      serverAdminUser = { };
+
+      desktopAdminUser = { };
+      desktopAdminDesc = { };
+
+      laptopAdminUser = { };
+      laptopAdminDesc = { };
+
+      tsKeyFilePath = { };
+      paperlessKeyFilePath = { };
+
+      borgKeyFilePath = { };
+      borgRshFilePath = { };
+      borgRepo = { };
+
+      plexDataDir = { };
+      testPath = { };
+      crowdsecApiKey = { };
+      sshPubKey = { };
+      gpgSshPubKey = { };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    age
+    sops
+  ];
 }
