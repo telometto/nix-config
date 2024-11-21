72121bd8d4dbf2419eb9b00d8a8883a86eee9372
telometto <65364211+telometto@users.noreply.github.com>
2024-11-21 16:05:19 +0100

    Changed to use sops.

--- modules/services/media/plex.nix
+++ modules/services/media/plex.nix
@@ -1,12 +1,12 @@
 # Host-specific system configuration defaults
-{ config, lib, pkgs, myVars, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   services.plex = {
     enable = true;
     openFirewall = true;
-    user = myVars.mainUsers.server.user; # If not set, the service will run as user "plex"
-    dataDir = myVars.server.plexDataDir; # If not set, the service will use the default data directory
+    user = config.sops.secrets.serverAdminUser.path; # If not set, the service will run as user "plex"
+    dataDir = config.sops.secrets.plexDataDir.path; # If not set, the service will use the default data directory
   };
 
   environment.systemPackages = with pkgs; [ plex ];
