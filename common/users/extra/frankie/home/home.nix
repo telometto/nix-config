e50f8d87f6d81d54d09ece2e8ef6369992860805
telometto <65364211+telometto@users.noreply.github.com>
2024-11-23 21:46:50 +0100

    First commit.

---
+++ common/users/extra/frankie/home/home.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, myVars, ... }:
+
+{
+  imports = [
+    ./programs.nix
+  ];
+
+  home = {
+    username = myVars.users.frankie.user;
+    stateVersion = "24.05";
+
+    packages = with pkgs; [
+      # Your packages here
+      atuin
+      bash
+      bat
+      #blesh
+      direnv
+      eza
+      #firefox
+      fzf
+      plasma5Packages.kdeconnect-kde
+      nix-direnv
+      sqlite
+      zoxide
+    ];
+  };
+
+  programs.home-manager.enable = true;
+}
