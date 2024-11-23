9b341307499c6c9dcb51deb1cf90ce258c7795a9
telometto <65364211+telometto@users.noreply.github.com>
2024-11-23 20:34:34 +0100

    Changed plymouth theme and added some `lib.mkIf`s.

--- common/boot/defaults.nix
+++ common/boot/defaults.nix
@@ -51,20 +51,19 @@
     plymouth = lib.mkIf (config.networking.hostName != myVars.server.hostname) {
       enable = true;
 
-      theme = "rings";
-      themePackages = with pkgs; [
-        # By default we would install all themes
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "rings" ];
-        })
-      ];
+      theme = "bgrt"; # Change to "rings" and uncomment the themePackages block to use the "rings" theme
+      # themePackages = with pkgs; [
+      #   # By default we would install all themes
+      #   (adi1090x-plymouth-themes.override {
+      #     selected_themes = [ "rings" ];
+      #   })
+      # ];
     };
 
-    /*
-      # Enable "Silent Boot"
-      consoleLogLevel = 0;
-      initrd.verbose = false;
-      kernelParams = [
+    # Enable "Silent Boot"
+    consoleLogLevel = lib.mkIf (config.networking.hostName != myVars.server.hostname) 0;
+    initrd.verbose = lib.mkIf (config.networking.hostName != myVars.server.hostname) false;
+    kernelParams = lib.mkIf (config.networking.hostName != myVars.server.hostname) [
       "quiet"
       "splash"
       "boot.shell_on_fail"
@@ -72,13 +71,12 @@
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
-      ];
+    ];
 
-      # Hide the OS choice for bootloaders.
-      # It's still possible to open the bootloader list by pressing any key
-      # It will just not appear on screen unless a key is pressed
-      loader.timeout = 0;
-      */
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = lib.mkIf (config.networking.hostName != myVars.server.hostname) 0;
   };
 
   services = {
