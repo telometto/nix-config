0c07568164ebc35cdaa26a0b56c0dbf2fc6a235a
telometto <65364211+telometto@users.noreply.github.com>
2024-11-25 17:07:57 +0100

    Added `lib.optionals` for desktop.

--- common/users/main/main-user.nix
+++ common/users/main/main-user.nix
@@ -5,7 +5,7 @@
   users.users."${myVars.users.admin.user}" = {
     description = myVars.users.admin.description;
     isNormalUser = myVars.users.admin.isNormalUser;
-    extraGroups = lib.concatLists myVars.users.admin.extraGroups (lib.mkIf config.networking.hostName == myVars.systems.desktop.hostname [ "openrazer" ]);
+    extraGroups = myVars.users.admin.extraGroups ++ lib.optionals (config.networking.hostName == myVars.systems.desktop.hostname) [ "openrazer" ];
     shell = pkgs.zsh;
 
     packages = with pkgs; [
