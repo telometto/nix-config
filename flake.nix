cc8f6d395fe51d5c4f3c3da91234b3fff2861334
telometto <65364211+telometto@users.noreply.github.com>
2024-11-29 15:46:17 +0100

    Major revamp.

--- flake.nix
+++ flake.nix
@@ -29,9 +29,7 @@
     };
 
     # VPN confinement repo
-    vpn-confinement = {
-      url = "github:Maroka-chan/VPN-Confinement";
-    };
+    vpn-confinement = { url = "github:Maroka-chan/VPN-Confinement"; };
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
@@ -59,6 +57,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    base16 = { url = "github:SenchoPens/base16.nix"; };
+
+    tt-schemes = {
+      url = "github:tinted-theming/schemes";
+      flake = false;
+    };
+
+    hyprland = { url = "github:hyprwm/Hyprland"; };
+
     # Nixarr repo (test)
     #nixarr.url = "github:rasmus-kirk/nixarr";
   };
@@ -67,7 +74,7 @@
     inputs@{ self
     , nixpkgs
     , nixpkgs-stable
-      # , nixpkgs-unstable
+    , nixpkgs-unstable
     , home-manager
     , nixos-hardware
     , lanzaboote
@@ -76,48 +83,69 @@
     , microvm
       # , crowdsec
       # , vpn-confinement
+    , hyprland
       # , nixarr
     , ...
     }:
     let
       myVars = import ./common/vars/vars.nix;
+
+      hostConfigs = {
+        snowfall = [ ./hosts/desktop/configuration.nix ];
+        blizzard = [ ./hosts/server/configuration.nix ];
+        stinkpad = [ ./hosts/laptop/configuration.nix ];
+      };
     in
     {
-      nixosConfigurations = {
-        blizzard = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-
-          modules = [
-            ./hosts/server/configuration.nix
-
-            # Start Home Manager configuration
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                sharedModules = [ inputs.sops-nix.homeManagerModules.sops ];
-
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                backupFileExtension = "hm-backup";
-
-                extraSpecialArgs = { inherit myVars; };
-
-                users.${myVars.users.admin.user} = import ./common/users/server/home/home.nix;
-              };
-            }
-          ];
-
-          specialArgs = {
-            inherit inputs myVars;
+      colmena = {
+        meta = {
+          # name = "homelab";
+          # description = "My colmena homelab";
+          nixpkgs = import nixpkgs {
+            system = "x86_64-linux";
+            overlays = [ ];
           };
+
+          specialArgs = { inherit inputs myVars; };
         };
 
+        # snowfall = ./hosts/desktop;
+        blizzard = ./hosts/server;
+        # stinkpad = ./hosts/laptop;
+      };
+
+      nixosConfigurations = {
         snowfall = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
 
           modules = [
-            ./hosts/desktop/configuration.nix
+            # Start Home Manager configuration
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                sharedModules = [
+                  inputs.sops-nix.homeManagerModules.sops
+                  inputs.hyprland.homeManagerModules.default
+                ];
 
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                backupFileExtension = "hm-backup";
+
+                extraSpecialArgs = { inherit inputs myVars; };
+
+                users.${myVars.users.admin.user} = import ./common/users/main/home/home.nix;
+              };
+            }
+          ] ++ hostConfigs.snowfall;
+
+          specialArgs = { inherit inputs myVars; };
+        };
+
+        blizzard = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+
+          modules = [
             # Start Home Manager configuration
             home-manager.nixosModules.home-manager
             {
@@ -130,22 +158,18 @@
 
                 extraSpecialArgs = { inherit myVars; };
 
-                users.${myVars.users.admin.user} = import ./common/users/main/home/home.nix;
+                users.${myVars.users.serverAdmin.user} = import ./common/users/server/home/home.nix;
               };
             }
-          ];
+          ] ++ hostConfigs.blizzard;
 
-          specialArgs = {
-            inherit inputs myVars;
-          };
+          specialArgs = { inherit inputs myVars; };
         };
 
         stinkpad = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
 
           modules = [
-            ./hosts/laptop/configuration.nix
-
             # Start Home Manager configuration
             home-manager.nixosModules.home-manager
             {
@@ -154,7 +178,7 @@
                 useUserPackages = true;
                 backupFileExtension = "hm-backup";
 
-                extraSpecialArgs = { inherit myVars; };
+                extraSpecialArgs = { inherit inputs myVars; };
 
                 users = {
                   ${myVars.users.admin.user} = import ./common/users/main/home/home.nix;
@@ -164,7 +188,7 @@
                 };
               };
             }
-          ];
+          ] ++ hostConfigs.stinkpad;
 
           specialArgs = {
             inherit inputs myVars;
