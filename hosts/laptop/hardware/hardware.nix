c2c47c0b1a1453ecc5e8be6e208778a73a6ca731
telometto <65364211+telometto@users.noreply.github.com>
2024-12-02 12:52:48 +0100

    Added several (commented) imports.

--- hosts/laptop/hardware/hardware.nix
+++ hosts/laptop/hardware/hardware.nix
@@ -1,5 +1,17 @@
 { config, inputs, lib, pkgs, ... }:
 
 {
-  imports = [ inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p51 ];
+  imports = [
+    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p51 # Lenovo ThinkPad P51 hardware configuration
+    # inputs.nixos-hardware.nixosModules.common-cpu-intel # Intel CPU microcode updates
+
+    # inputs.nixos-hardware.nixosModules.common-gpu-intel # Intel GPU drivers
+    # inputs.nixos-hardware.nixosModules.common-gpu-intel-kaby-lake # Intel Kaby Lake GPU drivers
+
+    # inputs.nixos-hardware.nixosModules.common-gpu-nvidia # NVIDIA GPU drivers
+
+    # inputs.nixos-hardware.nixosModules.common-pc-laptop # Laptop optimizations
+    # inputs.nixos-hardware.nixosModules.common-pc-laptop-acpi_call # ACPI call for laptop battery optimization
+    # inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd # SSD optimizations
+  ];
 }
