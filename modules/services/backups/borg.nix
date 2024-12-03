0b129040d273de546d8014062622c1834b9da957
telometto <65364211+telometto@users.noreply.github.com>
2024-12-03 12:46:41 +0100

    [TEST] `borg` setup.

--- modules/services/backups/borg.nix
+++ modules/services/backups/borg.nix
@@ -4,15 +4,17 @@
   services.borgbackup = lib.mkIf (config.networking.hostName == myVars.systems.server.hostname) {
     jobs = {
       homeserver = {
-        paths = "/home/${myVars.server.adminUser.user}/borgtest";
-        environment.BORG_RSH = "ssh -i $(cat${config.sops.secrets.borgRshFilePath.path})";
-        repo = "ssh://$(cat ${config.sops.secrets.borgRepo.path})";
+        paths = "/home/${myVars.users.serverAdmin.user}/borgtest";
+        # environment.BORG_RSH = "ssh -o 'StrictHostKeyChecking=no' -i ${config.sops.secrets.borgRshFilePath.path}";
+        environment.BORG_RSH = "ssh -o 'StrictHostKeyChecking=no' -i ${config.sops.secrets."general.borgRshFilePath".path}";
+        repo = "ssh://iu445agy@iu445agy.repo.borgbase.com/./repo";
         compression = "zstd,8";
         startAt = "daily";
 
         encryption = {
           mode = "repokey-blake2";
-          passCommand = "cat ${config.sops.secrets.borgKeyFilePath.path}";
+          # passCommand = "cat ${config.sops.secrets.borgKeyFilePath.path}";
+          passCommand = "cat ${config.sops.secrets."general/borgKeyFilePath".path}";
         };
       };
     };
