ab9c321ebeea561d0781492c968aa73cf0f49425
telometto </run/user/1000/secrets/git/github-email>
2024-12-05 14:38:07 +0100

    Changes to `username` and bumped `hm` version to `24.11`.

--- hosts/server/home/admin/home/home.nix
+++ hosts/server/home/admin/home/home.nix
@@ -11,8 +11,8 @@
   programs.home-manager.enable = true;
 
   home = {
-    username = VARS.users.serverAdmin.user; # Change this back upon reformatting
-    stateVersion = "24.05";
+    username = VARS.users.admin.user; # Change this back upon reformatting
+    stateVersion = "24.11";
 
     packages = with pkgs; [
       # Your packages here
