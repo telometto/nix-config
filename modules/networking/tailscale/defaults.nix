ca400bd7343ec4e046dd9b83213c89287a35c733
telometto <65364211+telometto@users.noreply.github.com>
2024-12-09 12:07:47 +0100

    [TESTING]

--- modules/networking/tailscale/defaults.nix
+++ modules/networking/tailscale/defaults.nix
@@ -24,7 +24,7 @@
       rules."50-tailscale" = {
         onState = [ "routable" ];
         script = ''
-          ${lib.getExe ethtool} -K eth0 rx-udp-gro-forwarding on rx-gro-list off
+          ${lib.getExe pkgs.ethtool} -K eth0 rx-udp-gro-forwarding on rx-gro-list off
         '';
       };
     };
