e26a24d2ad56a6ee8367c6ebb2349cb6aab353d5
telometto </run/user/1000/secrets/git/github-email>
2024-12-12 15:25:20 +0100

    testing

--- common/base/imports.nix
+++ common/base/imports.nix
@@ -3,40 +3,40 @@
 {
   imports = [
     # Boot
-    ./base/boot/defaults.nix
+    ./boot/defaults.nix
 
     # Environment
-    ./base/environment/defaults.nix
+    ./environment/defaults.nix
 
     # Filesystems
-    ./base/filesystems/defaults.nix
+    ./filesystems/defaults.nix
 
     # Hardware
-    ./base/hardware/firmware/defaults.nix
-    ./base/hardware/usb/defaults.nix
+    ./hardware/firmware/defaults.nix
+    ./hardware/usb/defaults.nix
 
     # Localization
-    ./base/i18n/defaults.nix
+    ./i18n/defaults.nix
 
     # Networking
-    ./base/networking/ssh/defaults.nix
-    ./base/networking/tools/defaults.nix
+    ./networking/ssh/defaults.nix
+    ./networking/tools/defaults.nix
 
     # Nix
-    ./base/nix/defaults.nix
+    ./nix/defaults.nix
 
     # Packages
-    ./base/packages/defaults.nix
+    ./packages/defaults.nix
 
     # Security
-    ./base/security/defaults.nix
-    ./base/security/gpg/defaults.nix
-    ./base/security/secrets/agenix.nix
-    ./base/security/secrets/sops-nix.nix
-    ./base/security/secureboot/lanzaboote.nix
+    ./security/defaults.nix
+    ./security/gpg/defaults.nix
+    ./security/secrets/agenix.nix
+    ./security/secrets/sops-nix.nix
+    ./security/secureboot/lanzaboote.nix
 
     # Utilities
-    ./base/utilities/memory/zram.nix
-    ./base/utilities/shell/defaults.nix
+    ./utilities/memory/zram.nix
+    ./utilities/shell/defaults.nix
   ];
 }
