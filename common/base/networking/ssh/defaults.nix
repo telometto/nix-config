9c983b6ed24a2a1878f58dfd6e907aeb4cee87a3
telometto </run/user/1000/secrets/git/github-email>
2024-12-14 00:36:16 +0100

    Testing banner.

--- common/base/networking/ssh/defaults.nix
+++ common/base/networking/ssh/defaults.nix
@@ -27,7 +27,27 @@
   services.openssh = {
     enable = true;
 
-    banner = "Testing stuff";
+    banner = ''
+               _nnnn_
+              dGGGGMMb
+             @p~qp~~qMb
+             M|@||@) M|
+             @,----.JM|
+            JS^\__/  qKL
+           dZP        qKRb
+          dZP          qKKb
+         fZP            SMMb
+         HZM            MMMM
+         FqM            MMMM
+       __| ".        |\dS"qML
+       |    `.       | `' \Zq
+      _)      \.___.,|     .'
+      \____   )MMMMMP|   .'
+           `-'       `--'
+      :: Welcome back to The Matrix! ::
+      :: Unauthorized access is prohibited ::
+      
+    '';
 
     settings = {
       X11Forwarding = false;
