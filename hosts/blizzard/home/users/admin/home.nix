d34f0c79f53e2a39e89056c0246d500be4f99a44
telometto </run/user/1000/secrets/git/github-email>
2024-12-15 20:57:16 +0100

    [FIX] Removed erroneous code block.

--- hosts/blizzard/home/users/admin/home.nix
+++ hosts/blizzard/home/users/admin/home.nix
@@ -19,19 +19,6 @@
     ./services/gpg/keyring.nix
   ];
 
-  users.users.${VARS.users.admin.user} = {
-    description = VARS.users.admin.description;
-    isNormalUser = VARS.users.admin.isNormalUser;
-    extraGroups = VARS.users.admin.extraGroups;
-    hashedPassword = VARS.users.admin.hashedPassword;
-    shell = pkgs.zsh;
-
-    openssh.authorizedKeys.keys = [
-      VARS.users.admin.sshPubKey
-      VARS.users.admin.gpgSshPubKey
-    ];
-  };
-
   programs.home-manager.enable = true;
 
   home = {
