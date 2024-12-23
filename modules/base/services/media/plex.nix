26add54b4750f5234f39976f0c7e337384599ac2
telometto <65364211+telometto@users.noreply.github.com>
2024-12-23 12:11:03 +0100

    [CHORE] Changed the data paths.

--- modules/base/services/media/plex.nix
+++ modules/base/services/media/plex.nix
@@ -6,7 +6,7 @@
     enable = true;
     openFirewall = true;
     # user = VARS.users.admin.user; # If not set, the service will run as user "plex"
-    dataDir = "/tank/apps/mediastack/nixos/plex"; # If not set, the service will use the default data directory
+    dataDir = "/rpool/unenc/apps/nixos/plex"; # If not set, the service will use the default data directory
   };
 
   environment.systemPackages = with pkgs; [ plex ];
