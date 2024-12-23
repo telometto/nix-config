02d4e5100a159cb9ea73ff1268bc502f31086001
telometto <65364211+telometto@users.noreply.github.com>
2024-12-23 12:18:21 +0100

    [TEST] Checking if each dataset needs to be mounted.

--- hosts/blizzard/boot/defaults.nix
+++ hosts/blizzard/boot/defaults.nix
@@ -81,15 +81,16 @@
       fsType = "zfs";
     };
 
-    "/rpool/unenc/dbs/mysql" = {
-      device = "rpool/unenc/dbs/mysql";
-      fsType = "zfs";
-    };
+    # Commented out for testing
+    # "/rpool/unenc/dbs/mysql" = {
+    #   device = "rpool/unenc/dbs/mysql";
+    #   fsType = "zfs";
+    # };
 
-    "/rpool/unenc/dbs/psql" = {
-      device = "rpool/unenc/dbs/psql";
-      fsType = "zfs";
-    };
+    # "/rpool/unenc/dbs/psql" = {
+    #   device = "rpool/unenc/dbs/psql";
+    #   fsType = "zfs";
+    # };
 
     "/rpool/unenc/media" = {
       device = "rpool/unenc/media";
