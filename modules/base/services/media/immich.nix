75488da51c87b1c1e82b6c292ffc71a1d598444c
telometto <65364211+telometto@users.noreply.github.com>
2024-12-23 13:32:14 +0100

    🔧 fix(formatting, paths): Fixed formatting and paths to reflect new fs setup.

--- modules/base/services/media/immich.nix
+++ modules/base/services/media/immich.nix
@@ -3,38 +3,43 @@
 
 {
   services.immich = {
-    enable = false;
+    enable = true;
 
-    host = "192.168.4.100"; # Default: "localhost"
-    #port = 2283; # Default: 3001
-    #openFirewall = true;
+    # host = "192.168.4.100"; # Default: "localhost"
+    # port = 2283; # Default: 3001
+    openFirewall = true;
 
-    #user = "immich";
-    #group = "immich";
+    # user = "immich";
+    # group = "immich";
 
-    secretsFile = "/opt/sec/immich-file"; # Default: null
-    mediaLocation = "/tank/apps/immich/library"; # Default: "/var/lib/immich"
+    # secretsFile = "/opt/sec/immich-file"; # Default: null
+    mediaLocation = "/flash/enc/personal/photos"; # Default: "/var/lib/immich"
 
     environment = {
       IMMICH_LOG_LEVEL = "verbose"; # Example
     };
 
+    settings = {
+      newVersionCheck.enabled = true;
+      # server.externalDomain = "";
+    };
+
     database = {
       enable = true; # Default: true
 
-      host = "/tank/apps/immich/postgres"; # Default: "/run/postgresql"
-      #port = 5432; # Default: 5432
+      host = "/rpool/unenc/dbs/psql/immich"; # Default: "/run/postgresql"
+      # port = 5432; # Default: 5432
 
-      #user = "immich"; # Default: "immich"
-      #name = "immich"; # Default: "immich"
-      #createDB = true; # Default: true
+      # user = "immich"; # Default: "immich"
+      # name = "immich"; # Default: "immich"
+      # createDB = true; # Default: true
     };
 
     redis = {
       enable = true;
 
-      #host = "192.168.2.100"; # Default: config.services.redis.servers.immich.unixSocket
-      #port = 0; # Default: 0
+      # host = "192.168.2.100"; # Default: config.services.redis.servers.immich.unixSocket
+      # port = 0; # Default: 0
     };
 
     machine-learning = {
@@ -45,4 +50,6 @@
       };
     };
   };
+
+  environment.systemPackages = with pkgs; [ immich ];
 }
