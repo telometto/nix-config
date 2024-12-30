e202702302b891fcf3748c343ab149b51cee1c4b
telometto </run/user/1000/secrets/git/github-email>
2024-12-30 09:40:42 +0100

    🔧 fix(`immich`): Forgot that server is on latest.

--- modules/base/services/media/immich.nix
+++ modules/base/services/media/immich.nix
@@ -1,5 +1,5 @@
 # Not in use yet; just have entered all the options as placeholders
-{ config, lib, pkgs, pkgs-stable, ... }:
+{ config, lib, pkgs, pkgs-stable-latest, ... }:
 
 {
   services.immich = {
@@ -54,5 +54,5 @@
 
   users.users.immich.extraGroups = [ "video" "render" ];
 
-  environment.systemPackages = with pkgs; [ pkgs-stable.immich ];
+  environment.systemPackages = with pkgs; [ pkgs-stable-latest.immich ];
 }
