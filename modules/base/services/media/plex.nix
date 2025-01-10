edf5a3a97eb0f604933939dc97ea5cb91b1bd3d8
telometto </run/user/1000/secrets/git/github-email>
2025-01-10 22:51:46 +0100

    Formatting.

--- modules/base/services/media/plex.nix
+++ modules/base/services/media/plex.nix
@@ -4,6 +4,7 @@
 {
   services.plex = {
     enable = true;
+
     openFirewall = true;
     # user = VARS.users.admin.user; # If not set, the service will run as user "plex"
     dataDir = "/rpool/unenc/apps/nixos/plex"; # If not set, the service will use the default data directory
