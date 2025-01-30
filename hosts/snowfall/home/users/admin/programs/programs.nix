e145afab2d3cbfc7ae8d43c682a2836f2fc37158
telometto <65364211+telometto@users.noreply.github.com>
2025-01-30 20:26:24 +0100

    🪄 feature(`zsh`): Added aliases.

--- hosts/snowfall/home/users/admin/programs/programs.nix
+++ hosts/snowfall/home/users/admin/programs/programs.nix
@@ -177,6 +177,20 @@ in
       # };
     };
 
+    zsh = {
+      shellAliases = {
+        # Kubernetes
+        k = "kubectl";
+        kap = "kubectl apply -f";
+        kdl = "kubectl delete";
+        kgt = "kubectl get";
+        kex = "kubectl exec -it";
+        klo = "kubectl logs";
+        kev = "kubectl events";
+        kds = "kubectl describe";
+      };
+    };
+
   };
 
   sops.secrets = {
