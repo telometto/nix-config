9eb8298c3bf282d3289a7cc29ae62265feae0481
telometto <65364211+telometto@users.noreply.github.com>
2025-01-30 20:40:53 +0100

    🔧 fix(`zsh`): Moved the aliases to the appropriate device.

--- hosts/blizzard/home/users/admin/programs/programs.nix
+++ hosts/blizzard/home/users/admin/programs/programs.nix
@@ -3,11 +3,21 @@
 {
   programs = {
     keychain = {
-      keys = [
-        "borg-blizzard"
-        "sops-hm-blizzard"
-        "zeno-blizzard"
-      ];
+      keys = [ "borg-blizzard" "sops-hm-blizzard" "zeno-blizzard" ];
+    };
+
+    zsh = {
+      shellAliases = {
+        # Kubernetes
+        k = "kubectl";
+        kap = "kubectl apply -f";
+        kdl = "kubectl delete";
+        kgt = "kubectl get";
+        kex = "kubectl exec -it";
+        klo = "kubectl logs";
+        kev = "kubectl events";
+        kds = "kubectl describe";
+      };
     };
   };
 }
--- hosts/snowfall/home/users/admin/programs/programs.nix
+++ hosts/snowfall/home/users/admin/programs/programs.nix
@@ -177,20 +177,6 @@ in
       # };
     };
 
-    zsh = {
-      shellAliases = {
-        # Kubernetes
-        k = "kubectl";
-        kap = "kubectl apply -f";
-        kdl = "kubectl delete";
-        kgt = "kubectl get";
-        kex = "kubectl exec -it";
-        klo = "kubectl logs";
-        kev = "kubectl events";
-        kds = "kubectl describe";
-      };
-    };
-
   };
 
   sops.secrets = {
