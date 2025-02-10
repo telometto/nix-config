75db9e18705400950a1ef4db5a371b0d850044ef
telometto <65364211+telometto@users.noreply.github.com>
2025-02-10 18:53:33 +0100

    Commented out ports.

--- hosts/snowfall/networking/defaults.nix
+++ hosts/snowfall/networking/defaults.nix
@@ -15,23 +15,22 @@ in {
     firewall = rec {
       enable = true;
 
-      allowedTCPPortRanges = [
-        {
-          # Required by KDE Connect
-          from = 1714;
-          to = 1764;
-        }
-        {
-          # Required for Steam LAN sharing; to be removed
-          from = 27031;
-          to = 27036;
-        }
-        {
-          # Required for WC3; to be removed
-          from = 6112;
-          to = 6119;
-        }
-      ];
+      allowedTCPPortRanges = [{
+        # Required by KDE Connect
+        from = 1714;
+        to = 1764;
+      }
+      # {
+      #   # Required for Steam LAN sharing; to be removed
+      #   from = 27031;
+      #   to = 27036;
+      # }
+      # {
+      #   # Required for WC3; to be removed
+      #   from = 6112;
+      #   to = 6119;
+      # }
+        ];
 
       allowedUDPPortRanges = allowedTCPPortRanges;
 
@@ -44,9 +43,9 @@ in {
         20048
         # End of NFS ports
 
-        27040 # Required for Steam LAN sharing; to be removed
-        27015
-        27020
+        # 27040 # Required for Steam LAN sharing; to be removed
+        # 27015
+        # 27020
       ];
 
       allowedUDPPorts = allowedTCPPorts;
