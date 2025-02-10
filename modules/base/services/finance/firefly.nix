8349b4c56f97195d8d71effbbc2a0f4d530f4d81
telometto <65364211+telometto@users.noreply.github.com>
2025-02-10 20:13:41 +0100

    Added more parameters.

--- modules/base/services/finance/firefly.nix
+++ modules/base/services/finance/firefly.nix
@@ -3,5 +3,12 @@
 {
   services.firefly-iii = {
     enable = true;
+
+    settings = {
+      APP_ENV = "local";
+      APP_KEY_FILE = "/opt/sec/ff-file";
+      enableNginx = true;
+      virtualHost = "firefly.blizzard.INTERNAL";
+    };
   };
 }
