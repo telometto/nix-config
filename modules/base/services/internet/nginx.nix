9909b9ced6c7c77642fbbe87efd2d46e2496de7e
telometto <65364211+telometto@users.noreply.github.com>
2025-02-10 20:33:11 +0100

    First commit.

---
+++ modules/base/services/internet/nginx.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.nginx = {
+    enable = true;
+
+    virtualHosts = {
+      "firefly.blizzard.INTERNAL" = {
+        enableACME = true;
+        forceSSL = false;
+        root = "/var/www/firefly";
+      };
+    };
+}
