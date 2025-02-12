f7760429d1b18d167154be2e058352f495740af4
telometto <65364211+telometto@users.noreply.github.com>
2025-02-12 12:46:58 +0100

    🧪 test(systemd): Timer fails to compile. Testing a change.

--- modules/base/system/defaults.nix
+++ modules/base/system/defaults.nix
@@ -22,23 +22,25 @@
     };
   };
 
-  systemd.services."nix-config-update" = {
-    description = "Update nix-config repository periodically";
-    serviceConfig = {
-      Type = "oneshot";
-      User = "zeno";
-      WorkingDirectory = "/home/zeno/.versioncontrol/github/projects/personal/nix-config";
-      ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.git}/bin/git fetch && ${pkgs.git}/bin/git pull'";
+  systemd = {
+    services."nix-config-update" = {
+      description = "Update nix-config repository periodically";
+      serviceConfig = {
+        Type = "oneshot";
+        User = "zeno";
+        WorkingDirectory = "/home/zeno/.versioncontrol/github/projects/personal/nix-config";
+        ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.git}/bin/git fetch && ${pkgs.git}/bin/git pull'";
+      };
     };
-  };
 
-  systemd.timers."nix-config-timer" = {
-    description = "Timer for updating the nix-config repository";
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "*-*-* *:15:00";
-      Persistent = true;
+    timers."nix-config-update" = {
+      description = "Timer for updating the nix-config repository";
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "*-*-* *:15:00";
+        Persistent = true;
+      };
+      service = "nix-config-update.service";
     };
-    service = "nix-config-update.service";
   };
 }
