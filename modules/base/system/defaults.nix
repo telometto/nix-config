fd46e80bba9ddeb9ef043307eeb8cc6a3b41a654
telometto <65364211+telometto@users.noreply.github.com>
2025-02-12 13:49:19 +0100

    Commented out systemd timers.

--- modules/base/system/defaults.nix
+++ modules/base/system/defaults.nix
@@ -22,25 +22,25 @@
     };
   };
 
-  systemd = {
-    services."nix-config-update" = {
-      description = "Update nix-config repository periodically";
-      serviceConfig = {
-        Type = "oneshot";
-        User = "zeno";
-        WorkingDirectory = "/home/zeno/.versioncontrol/github/projects/personal/nix-config";
-        ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.git}/bin/git fetch && ${pkgs.git}/bin/git pull'";
-      };
-    };
+  # systemd = {
+  #   services."nix-config-update" = {
+  #     description = "Update nix-config repository periodically";
+  #     serviceConfig = {
+  #       Type = "oneshot";
+  #       User = "zeno";
+  #       WorkingDirectory = "/home/zeno/.versioncontrol/github/projects/personal/nix-config";
+  #       ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.git}/bin/git fetch && ${pkgs.git}/bin/git pull'";
+  #     };
+  #   };
 
-    timers."nix-config-update" = {
-      description = "Timer for updating the nix-config repository";
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnCalendar = "*-*-* *:15:00";
-        Persistent = true;
-        Unit = "nix-config-update.service";
-      };
-    };
-  };
+  #   timers."nix-config-update" = {
+  #     description = "Timer for updating the nix-config repository";
+  #     wantedBy = [ "timers.target" ];
+  #     timerConfig = {
+  #       OnCalendar = "*-*-* *:15:00";
+  #       Persistent = true;
+  #       Unit = "nix-config-update.service";
+  #     };
+  #   };
+  # };
 }
