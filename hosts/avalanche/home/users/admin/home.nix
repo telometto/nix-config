7a606762265adb1da70da524bb83f044adac1f32
telometto <65364211+telometto@users.noreply.github.com>
2025-03-30 21:43:14 +0200

    Disabled `polychromatic`.

--- hosts/avalanche/home/users/admin/home.nix
+++ hosts/avalanche/home/users/admin/home.nix
@@ -72,7 +72,7 @@ in
       meld # Diff tool
       variety # Wallpaper changer
       zsh-powerlevel10k
-      polychromatic # Razer configuration tool
+      # polychromatic # Razer configuration tool
 
       # Gaming
       #mangohud
