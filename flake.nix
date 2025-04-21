2e29d66001d81ee49f99682f9c51169d89f1da0c
telometto <65364211+telometto@users.noreply.github.com>
2025-04-21 13:58:37 +0200

    🪄 feature(flake.nix): Major revamp to simplify code.

--- flake.nix
+++ flake.nix
@@ -1,48 +1,24 @@
-/**
- * This `flake.nix` file provides a comprehensive NixOS deployment configuration managed via Colmena.
- * It defines multiple host configurations and organizes inputs into categories such as Nixpkgs, Home Manager, Security, and more.
- * 
- * **Usage:**
- * - **Deploy Configurations:**
- *   Use `nixos-rebuild` or `colmena` commands to deploy or update host configurations.
- *   Example: `colmena deploy -f .#hostname`
- * 
- * - **Manage Inputs:**
- *   Modify inputs within their respective categories for updates and maintenance.
- * 
- * - **Handle Secrets:**
- *   Securely manage secrets via `nix-secrets`. Update the `vars.varsFile` as required.
- * 
- * - **Extend Configurations:**
- *   Add new host configurations by updating `hostConfigs` and user definitions.
- */
-
 {
   description = "Full-deployment NixOS flake";
 
   inputs = {
-    ## Nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable-latest.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
-    ## NixOS Hardware repo
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-    ## Home Manager repo
     home-manager = {
-      url = "github:nix-community/home-manager/master"; # Unstable
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ## Secure boot
     lanzaboote = {
       url = "github:nix-community/lanzaboote/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ## Secrets
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -53,147 +29,122 @@
       flake = true;
     };
 
-    ## Virtualization
     microvm = {
       url = "github:astro/microvm.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ## Security
     crowdsec = {
       url = "git+https://codeberg.org/kampka/nix-flake-crowdsec.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ## File systems
     disko = {
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ## Pimping
-    # base16 = { url = "github:SenchoPens/base16.nix"; };
+    hyprland.url = "github:hyprwm/Hyprland";
 
-    # tt-schemes = {
-    #   url = "github:tinted-theming/schemes";
-    #   flake = false;
-    # };
-
-    ## Desktop environments
-    hyprland = { url = "github:hyprwm/Hyprland"; };
-
-    ## NixOps
     colmena = {
       url = "github:zhaofengli/colmena";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    inputs@{ self
-    , nixpkgs
-    , nixpkgs-stable-latest
-    , nixpkgs-stable
-    , nixpkgs-unstable
-    , ...
-    }:
+  outputs = inputs@{ self, nixpkgs, ... }:
     let
+      lib = nixpkgs.lib;
       VARS = import inputs.nix-secrets.vars.varsFile;
-
-      ## Define host users
-      hostUsers = {
-        snowfall = [ VARS.users.admin.user ];
-        blizzard = [ VARS.users.admin.user ];
-        avalanche = [ VARS.users.admin.user ]; # VARS.users.luke.user ];
-        frostbite = [ VARS.users.admin.user ]; # Placeholder
-      };
-
-      ## Define host architecture
-      hostArch = {
-        snowfall = "x86_64-linux";
-        blizzard = "x86_64-linux";
-        avalanche = "x86_64-linux";
-        frostbite = "aarch64-linux"; # Placeholder
-      };
-
-      ## Define host configurations
-      hostConfigs = {
-        snowfall = [ ./hosts/snowfall/configuration.nix ];
-        blizzard = [ ./hosts/blizzard/configuration.nix ];
-        avalanche = [ ./hosts/avalanche/configuration.nix ];
-        # frostbite = [ ./hosts/frostbite/configuration.nix ]; # Placeholder
-      };
-
-      # Define a function to generate host configurations
-      mkConfig = host: nixpkgs.lib.nixosSystem rec {
-        system = hostArch.${host};
-
-        modules = [
-          ## Start Home Manager configuration
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "hm-backup";
-
-              sharedModules = [
-                inputs.sops-nix.homeManagerModules.sops
-                inputs.hyprland.homeManagerModules.default
-              ];
-
-              extraSpecialArgs = {
-                pkgs-stable-latest = import nixpkgs-stable-latest { inherit system; };
-                pkgs-stable = import nixpkgs-stable { inherit system; };
-                pkgs-unstable = import nixpkgs-unstable { inherit system; };
-
-                inherit inputs VARS;
-              };
-
-              users = nixpkgs.lib.genAttrs hostUsers.${host} (user:
-                import ./hosts/${host}/home/users/${
-                    if user == VARS.users.admin.user
-                    then "admin"
-                    else "extra/${user}"
-                  }/home.nix
-              );
-            };
-          }
-        ] ++ hostConfigs.${host};
-
-        specialArgs = {
-          pkgs-stable-latest = import nixpkgs-stable-latest { inherit system; };
-          pkgs-stable = import nixpkgs-stable { inherit system; };
-          pkgs-unstable = import nixpkgs-unstable { inherit system; };
-
-          inherit inputs VARS;
+      # gather hosts metadata in one place
+      hosts = {
+        snowfall = {
+          system = "x86_64-linux";
+          config = ./hosts/snowfall/configuration.nix;
+          users = [ VARS.users.admin.user ];
         };
+
+        blizzard = {
+          system = "x86_64-linux";
+          config = ./hosts/blizzard/configuration.nix;
+          users = [ VARS.users.admin.user ];
+        };
+
+        avalanche = {
+          system = "x86_64-linux";
+          config = ./hosts/avalanche/configuration.nix;
+          users = [ VARS.users.admin.user ];
+        };
+
       };
-    in
-    {
+
+      # read all overlays from ./overlays/*.nix
+      overlayFiles = builtins.attrNames (builtins.readDir ./overlays);
+      allOverlays = map (f: import (./overlays + "/" + f)) overlayFiles;
+    in {
+      # Colmena config
       colmena = {
         meta = {
-          # name = "homelab";
-          # description = "My colmena homelab";
           nixpkgs = import nixpkgs {
             system = "x86_64-linux";
-            overlays = [ ];
+            overlays = allOverlays;
           };
-
           specialArgs = { inherit inputs VARS; };
         };
-
-        snowfall = ./hosts/snowfall;
-        # blizzard = ./hosts/blizzard;
-        # avalanche = ./hosts/avalanche;
-        # frostbite = ./hosts/frostbite;
+        inherit (hosts) snowfall blizzard avalanche;
       };
 
-      nixosConfigurations = {
-        snowfall = mkConfig "snowfall";
-        blizzard = mkConfig "blizzard";
-        avalanche = mkConfig "avalanche";
-        # frostbite = mkConfig "frostbite"; # Placeholder
-      };
+      # NixOS configurations
+      nixosConfigurations = lib.mapAttrs (hostName: hostAttrs:
+        nixpkgs.lib.nixosSystem {
+          system = hostAttrs.system;
+          modules = [
+            inputs.home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                backupFileExtension = "hm-backup";
+                sharedModules = [
+                  inputs.sops-nix.homeManagerModules.sops
+                  inputs.hyprland.homeManagerModules.default
+                ];
+
+                extraSpecialArgs = {
+                  inherit inputs VARS;
+                  pkgs-stable-latest = import inputs.nixpkgs-stable-latest {
+                    system = hostAttrs.system;
+                  };
+                  pkgs-stable =
+                    import inputs.nixpkgs-stable { system = hostAttrs.system; };
+                  pkgs-unstable = import inputs.nixpkgs-unstable-small {
+                    system = hostAttrs.system;
+                  };
+                };
+
+                users = lib.genAttrs hostAttrs.users (user:
+                  import ./hosts/${hostName}/home/users/${
+                    if user == VARS.users.admin.user then
+                      "admin"
+                    else
+                      "extra/${user}"
+                  }/home.nix);
+              };
+            }
+            hostAttrs.config
+          ];
+
+          specialArgs = {
+            inherit inputs VARS;
+            pkgs-stable-latest = import inputs.nixpkgs-stable-latest {
+              system = hostAttrs.system;
+            };
+            pkgs-stable =
+              import inputs.nixpkgs-stable { system = hostAttrs.system; };
+            pkgs-unstable = import inputs.nixpkgs-unstable-small {
+              system = hostAttrs.system;
+            };
+          };
+        }) hosts;
     };
 }
