552d8e153a8be4eaf5a8cea7626f1aab1e5055ff
Agostino <65364211+telometto@users.noreply.github.com>
2025-10-03 21:17:53 +0200

    🪄 feature(cockpit): add openFirewall option to cockpit service configuration

--- modules/services/cockpit.nix
+++ modules/services/cockpit.nix
@@ -5,16 +5,21 @@ in
 {
   options.telometto.services.cockpit = {
     enable = lib.mkEnableOption "Cockpit web UI";
+
     port = lib.mkOption {
       type = lib.types.port;
       default = 9090;
+    };
+
+    openFirewall = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
     };
   };
   config = lib.mkIf cfg.enable {
     services.cockpit = {
       enable = true;
-      inherit (cfg) port;
-      openFirewall = true;
+      inherit (cfg) port openFirewall;
       settings.WebService.AllowUnencrypted = true;
     };
   };
