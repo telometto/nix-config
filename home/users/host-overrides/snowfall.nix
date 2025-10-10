05e8422859f9e7aac01214d00125c22dd16dc542
zeno <65364211+telometto@users.noreply.github.com>
2025-10-10 18:21:40 +0200

    🧹 chore(snowfall): remove development package configuration for vscode

--- home/users/host-overrides/snowfall.nix
+++ home/users/host-overrides/snowfall.nix
@@ -7,13 +7,4 @@
   # Example overrides:
   # hm.programs.development.extraPackages = with pkgs; [ snowfall-specific-tool ];
   # programs.git.extraConfig.snowfall = "specific-setting";
-
-  hm = {
-    programs = {
-      development.extraPackages = [
-        pkgs.vscode
-        # pkgs.jetbrains.idea-community-bin # disabled until lidbm issue has been solved
-      ];
-    };
-  };
 }
