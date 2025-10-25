7f18cb31cfa0703c6e6868477e9640a0e3c8b957
Agostino <65364211+telometto@users.noreply.github.com>
2025-10-25 11:27:10 +0200

    🔧 refactor(actual.nix): restructure options for clarity and organization

--- modules/services/actual.nix
+++ modules/services/actual.nix
@@ -3,11 +3,15 @@ let
   cfg = config.telometto.services.actual or { };
 in
 {
-  options.telometto.services.actual.enable = lib.mkEnableOption "Actual Budget";
-  options.telometto.services.actual.port = lib.mkOption {
-    type = lib.types.port;
-    default = 3838;
+  options.telometto.services.actual = {
+    enable = lib.mkEnableOption "Actual Budget";
+
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 3838;
+    };
   };
+
   config = lib.mkIf cfg.enable {
     services.actual = {
       enable = lib.mkDefault true;
