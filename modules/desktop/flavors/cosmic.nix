821d424c75c1f3fd1b68b5182b2de9e04291a773
zeno <65364211+telometto@users.noreply.github.com>
2025-10-30 21:56:53 +0100

    Added Cosmic desktop flavor

---
+++ modules/desktop/flavors/cosmic.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  flavor = config.telometto.desktop.flavor or "none";
+  is = v: flavor == v;
+in
+{
+  config = lib.mkIf (is "cosmic") {
+    services = {
+      xserver.enable = lib.mkDefault false;
+      desktopManager.cosmic.enable = true;
+
+      displayManager.cosmic-greeter = {
+        enable = true;
+      };
+    };
+
+    environment = {
+      sessionVariables = {
+        COSMIC_DATA_CONTROL_ENABLED = 1;
+      };
+
+      systemPackages = [ ];
+    };
+  };
+}
