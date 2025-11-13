d7cf4dc22a5b2175c7f2cf0e3850767391332234
zeno <65364211+telometto@users.noreply.github.com>
2025-11-13 17:23:59 +0100

    🧹 chore(nix.nix): format code

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -6,22 +6,27 @@
         "root"
         "@wheel"
       ];
+
       experimental-features = lib.mkDefault [
         "nix-command"
         "flakes"
       ];
+
       auto-optimise-store = lib.mkDefault true;
       download-buffer-size = lib.mkDefault 536870912;
     };
+
     gc = {
       automatic = lib.mkDefault true;
       dates = lib.mkDefault "weekly";
       options = lib.mkDefault "--delete-older-than 7d";
     };
+
     optimise = {
       automatic = lib.mkDefault true;
       dates = lib.mkDefault [ "02:00" ];
     };
+
     extraOptions = lib.mkDefault "!include ${config.sops.templates."access-tokens".path}";
   };
 }
