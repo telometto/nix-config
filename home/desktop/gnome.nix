b7050de96ea8e1c08f5306c6f797589db240a989
zeno <65364211+telometto@users.noreply.github.com>
2025-11-13 10:07:13 +0100

    🪄 feature(gnome): add coverflow-alt-tab GNOME extension

--- home/desktop/gnome.nix
+++ home/desktop/gnome.nix
@@ -93,6 +93,7 @@ in
       pkgs.gnomeExtensions.appindicator
       pkgs.gnomeExtensions.awesome-tiles
       pkgs.gnomeExtensions.burn-my-windows
+      pkgs.gnomeExtensions.coverflow-alt-tab
       pkgs.gnomeExtensions.clipboard-indicator
       pkgs.gnomeExtensions.gsconnect
       pkgs.gnomeExtensions.openweather-refined
