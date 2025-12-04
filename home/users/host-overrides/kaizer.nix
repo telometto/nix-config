39da7110823a891a6ca7aed08755faa2a86a63e3
zeno <65364211+telometto@users.noreply.github.com>
2025-12-04 20:21:43 +0100

    🧹 chore(kaizer): remove home.packages assignment and set `programs.ssh.enableDefaultConfig = false`

--- home/users/host-overrides/kaizer.nix
+++ home/users/host-overrides/kaizer.nix
@@ -1,6 +1,6 @@
 # Host-specific user overrides for kaizer
 # These settings will be applied to all users on kaizer host
-{ lib, pkgs, ... }:
+{ lib, ... }:
 {
-  home.packages = [ pkgs.variety ];
+  programs.ssh.enableDefaultConfig = false;
 }
