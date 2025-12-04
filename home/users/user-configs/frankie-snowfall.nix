a5cc224e989d694bdfe4dcb4310a7d1ef19bb83a
zeno <65364211+telometto@users.noreply.github.com>
2025-12-04 21:47:23 +0100

    🧹 chore(users): added hm conf for frankie on snowfall

---
+++ home/users/user-configs/frankie-snowfall.nix
@@ -0,0 +1,40 @@
+# User-specific configuration for frankie user on snowfall host
+# This file is automatically imported only for the frankie user on snowfall
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  LANGUAGE = "it_IT.UTF-8";
+{
+  # User-specific packages for frankie on snowfall
+  home.packages = [
+    pkgs.variety # Wallpaper changer
+    pkgs.polychromatic # Razer configuration tool
+  ];
+
+  home = {
+    language = {
+      address = lib.mkForce LANGUAGE;
+      base = lib.mkForce LANGUAGE;
+      collate = lib.mkForce LANGUAGE;
+      ctype = lib.mkForce LANGUAGE;
+      measurement = lib.mkForce LANGUAGE;
+      messages = lib.mkForce LANGUAGE;
+      monetary = lib.mkForce LANGUAGE;
+      name = lib.mkForce LANGUAGE;
+      numeric = lib.mkForce LANGUAGE;
+      paper = lib.mkForce LANGUAGE;
+      telephone = lib.mkForce LANGUAGE;
+      time = lib.mkForce LANGUAGE;
+    };
+  };
+
+  hm = {
+    programs = {
+      media.jf-mpv.enable = false;
+    };
+  };
+}
