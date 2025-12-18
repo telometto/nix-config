04b4ce706b72f141c52b1e53aee5036ea4cd8365
zeno <65364211+telometto@users.noreply.github.com>
2025-12-18 23:03:57 +0100

    🧹 chore(base.nix): re-enable element-desktop

--- home/base.nix
+++ home/base.nix
@@ -47,7 +47,7 @@ in
 
         social = {
           enable = lib.mkDefault true;
-          element-desktop.enable = lib.mkDefault false; # Disabled for now, due to issues with libolm
+          element-desktop.enable = lib.mkDefault true;
           vesktop.enable = lib.mkDefault true;
         };
 
