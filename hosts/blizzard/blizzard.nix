0fb5cf68fb780f6d818efcce99311ab4604c0084
zeno <65364211+telometto@users.noreply.github.com>
2025-12-19 22:43:50 +0100

    🧹 chore(blizzard.nix): enable openFirewall for samba service

--- hosts/blizzard/blizzard.nix
+++ hosts/blizzard/blizzard.nix
@@ -64,6 +64,9 @@ in
 
       samba = {
         enable = true;
+
+        openFirewall = true;
+
         shares.destroyme = {
           path = "/rpool/unenc/destroyme";
           forceUser = "zeno";
