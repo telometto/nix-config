ae1bab4f6b6c452d387d4c003f38fa6f6d78a959
Zeno <65364211+telometto@users.noreply.github.com>
2025-12-22 13:19:42 +0100

    🔧 fix(flatpak.nix): remove xdg.portal configuration

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -1,4 +1,3 @@
-# OK
 {
   lib,
   config,
@@ -20,12 +19,15 @@ in
   config = lib.mkIf cfg.enable {
     services.flatpak.enable = true;
 
-    xdg.portal = {
-      enable = lib.mkDefault true;
-      extraPortals = lib.mkDefault [ pkgs.xdg-desktop-portal-gtk ];
-      xdgOpenUsePortal = lib.mkDefault true;
-      config.common.default = lib.mkDefault "*";
-    };
+    # NOTE: xdg.portal configuration is NOT done here.
+    # Each desktop environment (GNOME, KDE/Plasma, Hyprland, etc.) should configure
+    # its own portal in its respective module:
+    # - GNOME: automatically configures xdg-desktop-portal-gnome
+    # - KDE/Plasma: automatically configures xdg-desktop-portal-kde
+    # - Hyprland: configured in modules/desktop/flavors/hyprland.nix
+    #
+    # See: https://nixos.org/manual/nixos/stable/#module-services-flatpak
+    # and: https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/config/xdg/portal.nix
 
     systemd.services.flatpak-repo = lib.mkMerge [
       {
@@ -33,6 +35,8 @@ in
         path = [ pkgs.flatpak ];
         script = ''
           flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+          flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+          flatpak remote-add --if-not-exists --user cosmic https://apt.pop-os.org/cosmic/cosmic.flatpakrepo
         '';
       }
       cfg.unitOverrides
