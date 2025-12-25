decbfe99c919762f919fd5dab257292b63d6bfb8
zeno <65364211+telometto@users.noreply.github.com>
2025-12-25 20:47:26 +0100

    🧹 chore(kaizer): moved user-specific configurations to host-override

--- home/users/host-overrides/kaizer.nix
+++ home/users/host-overrides/kaizer.nix
@@ -2,5 +2,19 @@
 # These settings will be applied to all users on kaizer host
 { lib, ... }:
 {
+  home.packages = [ pkgs.variety ];
+
+  hm = {
+    programs = {
+      media = {
+        enable = true;
+
+        mpv.enable = true;
+        yt-dlp.enable = true;
+        jf-mpv.enable = lib.mkForce false;
+      };
+    };
+  };
+
   programs.ssh.enableDefaultConfig = false;
 }
--- home/users/user-configs/luke-kaizer.nix
+++ home/users/user-configs/luke-kaizer.nix
@@ -1,23 +1,3 @@
 # User-specific configuration for luke on kaizer host
 # This file is automatically imported only for luke user on kaizer
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}:
-{
-  home.packages = [ pkgs.variety ];
-
-  hm = {
-    programs = {
-      media = {
-        enable = true;
-
-        mpv.enable = true;
-        yt-dlp.enable = true;
-        jf-mpv.enable = false;
-      };
-    };
-  };
-}
+{ ... }: { }
