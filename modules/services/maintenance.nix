c4c38a00078daeca385abbffff5034f7095f33b9
zeno <65364211+telometto@users.noreply.github.com>
2026-01-01 10:51:15 +0100

    🧹 chore(udevil): apply GCC 15 fix for udevil build failure in overlays.nix

--- modules/services/maintenance.nix
+++ modules/services/maintenance.nix
@@ -10,7 +10,7 @@ in
     services = {
       fstrim.enable = true;
       fwupd.enable = true;
-      # devmon.enable = true; # FIXME: Disabled due to udevil build failure - https://github.com/NixOS/nixpkgs/issues/475479
+      devmon.enable = true; # FIXME: udevil GCC 15 fix applied via overlay in overlays.nix due to udevil build failure - https://github.com/NixOS/nixpkgs/issues/475479
       gvfs.enable = true;
       udisks2.enable = true;
       zram-generator.enable = true;
