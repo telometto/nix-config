3ede95d8e77478ff0c6d08a8e5f3f6c34b1a155e
zeno <65364211+telometto@users.noreply.github.com>
2026-01-01 10:30:01 +0100

    🧹 chore(maintenance): comment out devmon service due to build failure

--- modules/services/maintenance.nix
+++ modules/services/maintenance.nix
@@ -5,12 +5,12 @@ let
 in
 {
   options.telometto.services.maintenance.enable =
-    lib.mkEnableOption "Basic maintenance timers/services (fstrim, fwupd, zram, devmon, gvfs, udisks2)";
+    lib.mkEnableOption "Basic maintenance timers/services (fstrim, fwupd, zram, gvfs, udisks2)";
   config = lib.mkIf cfg.enable {
     services = {
       fstrim.enable = true;
       fwupd.enable = true;
-      devmon.enable = true;
+      # devmon.enable = true; # FIXME: Disabled due to udevil build failure - https://github.com/NixOS/nixpkgs/issues/475479
       gvfs.enable = true;
       udisks2.enable = true;
       zram-generator.enable = true;
