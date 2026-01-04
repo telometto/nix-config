2fe497555ee269ae19bdc24e3a850dbe1af6e86a
zeno <65364211+telometto@users.noreply.github.com>
2026-01-04 19:11:21 +0100

    🧹 chore(snowfall.nix): disable chromium browser configuration for all users

--- home/users/host-overrides/snowfall.nix
+++ home/users/host-overrides/snowfall.nix
@@ -8,6 +8,9 @@
 {
   # Snowfall-specific user configuration
   # These settings will be applied to all users on this host
+  hm = {
+    programs.browsers.chromium.enable = lib.mkForce false;
+  };
 
   programs.ssh = {
     enableDefaultConfig = false;
