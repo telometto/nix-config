a30a3f2bbc5354497c881f8db9d06aadf0b6d03e
Zeno <65364211+telometto@users.noreply.github.com>
2026-01-06 12:22:33 +0100

    🔧 fix(ups-monitoring): remove low battery charge directive and enable outlet status checks

--- hosts/blizzard/blizzard.nix
+++ hosts/blizzard/blizzard.nix
@@ -145,7 +145,6 @@ in
           description = "Eaton 9130 UPS";
           directives = [
             "pollinterval = 5" # Poll every 5 seconds
-            "battery.charge.low = 30"
             "battery.runtime.low = 600"
           ];
         };
@@ -178,12 +177,12 @@ in
             "input.frequency"
             "input.voltage"
             "input.voltage.nominal"
-            # "outlet.1.status"
-            # "outlet.2.status"
-            # "outlet.1.switchable"
-            # "outlet.2.switchable"
-            # "outlet.1.delay.shutdown"
-            # "outlet.2.delay.shutdown"
+            "outlet.1.status"
+            "outlet.2.status"
+            "outlet.1.switchable"
+            "outlet.2.switchable"
+            "outlet.1.delay.shutdown"
+            "outlet.2.delay.shutdown"
             "output.current"
             "output.frequency"
             "output.voltage"
