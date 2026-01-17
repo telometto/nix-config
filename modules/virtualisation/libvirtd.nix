3fa6efa3e24dad74295d3b2e083f6fe6b462e660
zeno <65364211+telometto@users.noreply.github.com>
2026-01-17 14:34:29 +0100

    🔧 fix(libvirtd.nix): remove deprecated OVMF configuration from VM settings

--- modules/virtualisation/libvirtd.nix
+++ modules/virtualisation/libvirtd.nix
@@ -25,10 +25,6 @@ in
         package = pkgs.qemu_kvm;
         runAsRoot = false;
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [ pkgs.OVMFFull.fd ];
-        };
       };
     };
 
