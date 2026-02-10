f2e28f2d844f66b7640a19e98241beb482d39e4e
Zeno <65364211+telometto@users.noreply.github.com>
2026-02-10 11:45:34 +0100

    🔧 fix(wireguard): refactor postUp and preDown commands to use package binaries

--- vms/wireguard.nix
+++ vms/wireguard.nix
@@ -99,12 +99,32 @@
     mtu = 1390;
     dns = [ "1.1.1.1" ];
     addresses = [ "10.13.128.81/24" ];
-    postUp = [
-      "DROUTE=$(ip route | grep default | awk '{print $3}'); HOMENET=192.168.0.0/16; HOMENET2=10.0.0.0/8; HOMENET3=172.16.0.0/12; ip route add $HOMENET3 via $DROUTE; ip route add $HOMENET2 via $DROUTE; ip route add $HOMENET via $DROUTE; iptables -I OUTPUT -d $HOMENET -j ACCEPT; iptables -A OUTPUT -d $HOMENET2 -j ACCEPT; iptables -A OUTPUT -d $HOMENET3 -j ACCEPT; iptables -A OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT"
-    ];
-    preDown = [
-      "DROUTE=$(ip route | grep default | awk '{print $3}'); HOMENET=192.168.0.0/16; HOMENET2=10.0.0.0/8; HOMENET3=172.16.0.0/12; ip route del $HOMENET3 via $DROUTE; ip route del $HOMENET2 via $DROUTE; ip route del $HOMENET via $DROUTE; iptables -D OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT; iptables -D OUTPUT -d $HOMENET -j ACCEPT; iptables -D OUTPUT -d $HOMENET2 -j ACCEPT; iptables -D OUTPUT -d $HOMENET3 -j ACCEPT"
-    ];
+    postUp = ''
+      DROUTE=$(${pkgs.iproute2}/bin/ip route | ${pkgs.gnugrep}/bin/grep default | ${pkgs.gawk}/bin/awk '{print $3}')
+      HOMENET=192.168.0.0/16
+      HOMENET2=10.0.0.0/8
+      HOMENET3=172.16.0.0/12
+      ${pkgs.iproute2}/bin/ip route add $HOMENET3 via $DROUTE
+      ${pkgs.iproute2}/bin/ip route add $HOMENET2 via $DROUTE
+      ${pkgs.iproute2}/bin/ip route add $HOMENET via $DROUTE
+      ${pkgs.iptables}/bin/iptables -I OUTPUT -d $HOMENET -j ACCEPT
+      ${pkgs.iptables}/bin/iptables -A OUTPUT -d $HOMENET2 -j ACCEPT
+      ${pkgs.iptables}/bin/iptables -A OUTPUT -d $HOMENET3 -j ACCEPT
+      ${pkgs.iptables}/bin/iptables -A OUTPUT ! -o %i -m mark ! --mark $(${pkgs.wireguard-tools}/bin/wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT
+    '';
+    preDown = ''
+      DROUTE=$(${pkgs.iproute2}/bin/ip route | ${pkgs.gnugrep}/bin/grep default | ${pkgs.gawk}/bin/awk '{print $3}')
+      HOMENET=192.168.0.0/16
+      HOMENET2=10.0.0.0/8
+      HOMENET3=172.16.0.0/12
+      ${pkgs.iproute2}/bin/ip route del $HOMENET3 via $DROUTE
+      ${pkgs.iproute2}/bin/ip route del $HOMENET2 via $DROUTE
+      ${pkgs.iproute2}/bin/ip route del $HOMENET via $DROUTE
+      ${pkgs.iptables}/bin/iptables -D OUTPUT ! -o %i -m mark ! --mark $(${pkgs.wireguard-tools}/bin/wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT
+      ${pkgs.iptables}/bin/iptables -D OUTPUT -d $HOMENET -j ACCEPT
+      ${pkgs.iptables}/bin/iptables -D OUTPUT -d $HOMENET2 -j ACCEPT
+      ${pkgs.iptables}/bin/iptables -D OUTPUT -d $HOMENET3 -j ACCEPT
+    '';
     peers = [
       {
         publicKey = "<REDACTED>";
