3c918a7766e989510efa18dab92cbfb157d18bfa
Zeno <65364211+telometto@users.noreply.github.com>
2026-02-13 13:46:59 +0100

    🔧 fix(gitea): update reverse proxy condition to check for domain presence

--- modules/services/gitea.nix
+++ modules/services/gitea.nix
@@ -255,7 +255,7 @@ in
     };
 
     services.traefik.dynamic.files.gitea =
-      lib.mkIf (cfg.reverseProxy.enable && config.services.traefik.enable or false)
+      lib.mkIf (cfg.reverseProxy.enable && cfg.reverseProxy.domain != null && config.services.traefik.enable or false)
         {
           settings = {
             http = {
