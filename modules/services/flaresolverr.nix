2024bfc8318940fc014e5361955af3d83add886e
Zeno <65364211+telometto@users.noreply.github.com>
2026-02-13 11:40:43 +0100

    🔧 fix(flaresolverr): refactor configuration by removing user and group options and simplifying service setup

--- modules/services/flaresolverr.nix
+++ modules/services/flaresolverr.nix
@@ -1,11 +1,12 @@
 { lib, config, pkgs, ... }:
 let
   cfg = config.sys.services.flaresolverr;
-  port = toString cfg.port;
 in
 {
   options.sys.services.flaresolverr = {
     enable = lib.mkEnableOption "FlareSolverr";
+
+    package = lib.mkPackageOption pkgs "flaresolverr" { };
 
     port = lib.mkOption {
       type = lib.types.port;
@@ -25,16 +26,6 @@ in
       description = "Log level for FlareSolverr.";
     };
 
-    user = lib.mkOption {
-      type = lib.types.str;
-      default = "flaresolverr";
-    };
-
-    group = lib.mkOption {
-      type = lib.types.str;
-      default = "flaresolverr";
-    };
-
     openFirewall = lib.mkOption {
       type = lib.types.bool;
       default = false;
@@ -42,33 +33,14 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    users.groups.${cfg.group} = { };
-
-    users.users.${cfg.user} = {
-      isSystemUser = true;
-      group = cfg.group;
+    services.flaresolverr = {
+      enable = true;
+      inherit (cfg) package port openFirewall;
     };
 
-    systemd.services.flaresolverr = {
-      description = "FlareSolverr";
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        User = cfg.user;
-        Group = cfg.group;
-        ExecStart = "${pkgs.flaresolverr}/bin/flaresolverr";
-        Restart = "on-failure";
-        Environment = [
-          "HOST=${cfg.bind}"
-          "PORT=${port}"
-          "LOG_LEVEL=${cfg.logLevel}"
-        ];
-      };
-    };
-
-    networking.firewall = lib.mkIf cfg.openFirewall {
-      allowedTCPPorts = [ cfg.port ];
+    systemd.services.flaresolverr.environment = {
+      HOST = cfg.bind;
+      LOG_LEVEL = cfg.logLevel;
     };
   };
 }
