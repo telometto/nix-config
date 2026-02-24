b75bf51769c893860d9ac4703f2ef0d733d16720
zeno <65364211+telometto@users.noreply.github.com>
2026-02-24 20:16:45 +0100

    🧹 chore(blizzard.nix): update overlays.fromInputs to replace cloud-hypervisor with beets

--- hosts/blizzard/blizzard.nix
+++ hosts/blizzard/blizzard.nix
@@ -51,7 +51,7 @@
       # TODO: Remove once microvm.nix adds format=raw to cloud-hypervisor disk args
       # cloud-hypervisor v51.0 blocks sector-0 writes for autodetected raw images
       # (security fix PR #7728), breaking all MicroVM volume mounts
-      nixpkgs-stable-latest = [ "cloud-hypervisor" ];
+      nixpkgs-stable-latest = [ "beets" ];
     };
 
     services = {
