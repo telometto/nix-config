4288c0e636cd68ece7219704615653dad3ea9a21
Zeno <65364211+telometto@users.noreply.github.com>
2026-02-26 13:14:16 +0100

    🧹 chore(matrix-synapse): add configuration to avoid proxy_headers_hash warnings

--- vms/matrix-synapse.nix
+++ vms/matrix-synapse.nix
@@ -294,6 +294,12 @@
     recommendedOptimisation = true;
     recommendedGzipSettings = true;
 
+    # Avoid warning about proxy_headers_hash with recommended settings
+    appendHttpConfig = ''
+      proxy_headers_hash_max_size 1024;
+      proxy_headers_hash_bucket_size 128;
+    '';
+
     virtualHosts."matrix" = {
       listen = [
         {
