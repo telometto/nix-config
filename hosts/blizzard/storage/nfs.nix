a48bd55ab68411db075d9eb0335bed2bdce2fc99
zeno <65364211+telometto@users.noreply.github.com>
2026-03-02 22:22:24 +0100

    🧹 chore(nfs.nix): comment out exports configuration (zfs managed)

--- hosts/blizzard/storage/nfs.nix
+++ hosts/blizzard/storage/nfs.nix
@@ -5,10 +5,10 @@
     server = {
       enable = true;
       openFirewall = lib.mkForce true;
-      exports = ''
-        /rpool/enc/transfers 192.168.2.0/24(rw,sync,nohide,no_subtree_check)
-        /rpool/unenc/media/data 10.100.0.0/24(rw,sync,nohide,no_subtree_check)
-      '';
+      # exports = ''
+      #   /rpool/enc/transfers 192.168.2.0/24(rw,sync,nohide,no_subtree_check)
+      #   /rpool/unenc/media/data 10.100.0.0/24(rw,sync,nohide,no_subtree_check)
+      # '';
     };
   };
 }
