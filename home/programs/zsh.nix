f8f95ef7cb3351f7a305e1442dff3d2edc243f3e
Zeno <65364211+telometto@users.noreply.github.com>
2026-03-06 17:14:51 +0100

    🔧 fix(zsh.nix): added semicolons for extracts

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -96,18 +96,18 @@ in
         extract = ''
           if [[ -f "$1" ]]; then
             case "$1" in
-              *.tar.bz2) tar xjf "$1" ;
-              *.tar.gz)  tar xzf "$1" ;
-              *.tar.xz)  tar xJf "$1" ;
-              *.bz2)     bunzip2 "$1" ;
-              *.gz)      gunzip "$1" ;
-              *.tar)     tar xf "$1" ;
-              *.tbz2)    tar xjf "$1" ;
-              *.tgz)     tar xzf "$1" ;
-              *.zip)     unzip "$1" ;
-              *.Z)       uncompress "$1" ;
-              *.7z)      7z x "$1" ;
-              *)         echo "Cannot extract '$1'" ;
+              *.tar.bz2) tar xjf "$1" ;;
+              *.tar.gz)  tar xzf "$1" ;;
+              *.tar.xz)  tar xJf "$1" ;;
+              *.bz2)     bunzip2 "$1" ;;
+              *.gz)      gunzip "$1" ;;
+              *.tar)     tar xf "$1" ;;
+              *.tbz2)    tar xjf "$1" ;;
+              *.tgz)     tar xzf "$1" ;;
+              *.zip)     unzip "$1" ;;
+              *.Z)       uncompress "$1" ;;
+              *.7z)      7z x "$1" ;;
+              *)         echo "Cannot extract '$1'" ;;
             esac
           else
             echo "'$1' is not a valid file"
