4d5b62b0a1fad37ca10be16bc5aaa7c3d8d1e14d
zeno <65364211+telometto@users.noreply.github.com>
2026-03-07 17:33:27 +0100

    🔧 fix(backup.nix):
    - replaced `StrictHostKeyChecking=accept-new` with strict checking
    - added a dedicated `UserKnownHostsFile` path: `~/.ssh/known_hosts-borg-blizzard`

--- hosts/blizzard/services/backup.nix
+++ hosts/blizzard/services/backup.nix
@@ -1,11 +1,14 @@
 { config, VARS, ... }:
+let
+  borgKnownHostsFile = "/home/${VARS.users.zeno.user}/.ssh/known_hosts-borg-blizzard";
+in
 {
   sys.services.borgbackup = {
     enable = false;
 
     jobs.homeserver = {
       paths = [ "/home/${VARS.users.zeno.user}" ];
-      environment.BORG_RSH = "ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=accept-new -i /home/${VARS.users.zeno.user}/.ssh/borg-blizzard";
+      environment.BORG_RSH = "ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=yes -o UserKnownHostsFile=${borgKnownHostsFile} -i /home/${VARS.users.zeno.user}/.ssh/borg-blizzard";
       repo = config.sys.secrets.borgRepo or "ssh://iu445agy@iu445agy.repo.borgbase.com/./repo";
       compression = "zstd,8";
       startAt = "daily";
