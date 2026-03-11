60d0fbefa51482a06c97c5869c68cbaa381400e8
Zeno <65364211+telometto@users.noreply.github.com>
2026-03-11 10:27:15 +0100

    🧹 chore(maintenance.nix): restructure maintenance service options and improve zram configuration

--- modules/services/maintenance.nix
+++ modules/services/maintenance.nix
@@ -3,16 +3,44 @@ let
   cfg = config.sys.services.maintenance;
 in
 {
-  options.sys.services.maintenance.enable =
-    lib.mkEnableOption "Basic maintenance timers/services (fstrim, fwupd, zram, gvfs, udisks2)";
+  options.sys.services.maintenance = {
+    enable =
+      lib.mkEnableOption "Basic maintenance timers/services (fstrim, firmware updates, zram, removable-media helpers)";
+
+    desktop.enable = lib.mkEnableOption "desktop-oriented removable-media helpers (devmon, gvfs, udisks2)";
+
+    firmware.enable = lib.mkEnableOption "firmware updates via fwupd";
+
+    zram = {
+      enable = lib.mkEnableOption "compressed in-memory swap";
+
+      algorithm = lib.mkOption {
+        type = lib.types.str;
+        default = "zstd";
+        description = "Compression algorithm used by zram swap.";
+      };
+
+      memoryPercent = lib.mkOption {
+        type = lib.types.ints.positive;
+        default = 25;
+        description = "Maximum zram swap size as a percentage of total RAM.";
+      };
+    };
+  };
+
   config = lib.mkIf cfg.enable {
     services = {
       fstrim.enable = true;
-      fwupd.enable = true;
-      devmon.enable = true; # FIXME: udevil GCC 15 fix applied via overlay in overlays.nix due to udevil build failure - https://github.com/NixOS/nixpkgs/issues/475479
-      gvfs.enable = true;
-      udisks2.enable = true;
-      zram-generator.enable = true;
+      fwupd.enable = cfg.firmware.enable;
+      devmon.enable = cfg.desktop.enable; # FIXME: udevil GCC 15 fix applied via overlay in overlays.nix due to udevil build failure - https://github.com/NixOS/nixpkgs/issues/475479
+      gvfs.enable = cfg.desktop.enable;
+      udisks2.enable = cfg.desktop.enable;
+    };
+
+    zramSwap = lib.mkIf cfg.zram.enable {
+      enable = true;
+      algorithm = cfg.zram.algorithm;
+      memoryPercent = cfg.zram.memoryPercent;
     };
   };
 }
