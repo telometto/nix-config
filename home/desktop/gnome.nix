4a6583e2ade393dc184365c789ec4c75a683a085
zeno <65364211+telometto@users.noreply.github.com>
2026-03-27 16:22:42 +0100

    🧹 chore(gnome.nix): refactor gtk4 configuration to include theme

--- home/desktop/gnome.nix
+++ home/desktop/gnome.nix
@@ -131,8 +131,11 @@ in
       gtk3.extraConfig = {
         gtk-application-prefer-dark-theme = 1;
       };
-      gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
+      gtk4 = {
+        theme = null;
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
       };
     };
 
