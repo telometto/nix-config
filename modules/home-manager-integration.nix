8c4596fd2f42fabcae5286458d4b967e805f537f
zeno <65364211+telometto@users.noreply.github.com>
2026-04-06 13:59:05 +0200

    fix(home-manager): revert backupFileExtension format to default value

--- modules/home-manager-integration.nix
+++ modules/home-manager-integration.nix
@@ -13,7 +13,7 @@ in
     home-manager = {
       useGlobalPkgs = lib.mkDefault true;
       useUserPackages = lib.mkDefault true;
-      backupFileExtension = lib.mkDefault "hm-backup-${config.system.nixos.label}";
+      backupFileExtension = lib.mkDefault "hm-backup";
 
       sharedModules = [
         inputs.sops-nix.homeManagerModules.sops
