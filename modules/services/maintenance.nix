96d4506095c3f354637a139bb4f086c6358d7ba0
Zeno <65364211+telometto@users.noreply.github.com>
2026-04-18 22:33:20 +0200

    fix(maintenance): remove outdated comment regarding devmon enablement

--- modules/services/maintenance.nix
+++ modules/services/maintenance.nix
@@ -9,7 +9,7 @@ in
     services = {
       fstrim.enable = true;
       fwupd.enable = true;
-      devmon.enable = true; # FIXME: udevil GCC 15 fix applied via overlay in overlays.nix due to udevil build failure - https://github.com/NixOS/nixpkgs/issues/475479
+      devmon.enable = true;
       gvfs.enable = true;
       udisks2.enable = true;
       zram-generator.enable = true;
