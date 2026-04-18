939c4f8d1d8c1c1535ab41a1e6a2b5f995a11cf0
Zeno <65364211+telometto@users.noreply.github.com>
2026-04-18 22:47:46 +0200

    refactor(constants): remove hardcoded cloudflare accountId and policyId for improved security

--- lib/constants.nix
+++ lib/constants.nix
@@ -2,9 +2,4 @@
 # For secrets use sops-nix; for per-host data use host configs.
 {
   tailscale.suffix = "mole-delta.ts.net";
-
-  cloudflare = {
-    accountId = "1f65156829c5e18a3648609b381dec9c";
-    policyId = "897e5beb-2937-448f-a444-4b51ff7479b0";
-  };
 }
