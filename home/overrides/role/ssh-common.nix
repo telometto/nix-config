82685adc1c2bf8032a05ba5185798373899a9459
zeno <65364211+telometto@users.noreply.github.com>
2026-06-25 10:36:23 +0200

    refactor(home/overrides): remove host-scoped shared SSH override module

---
+++ home/overrides/role/ssh-common.nix
@@ -0,0 +1,31 @@
+{ config, ... }:
+{
+  programs.ssh = {
+    enableDefaultConfig = false;
+
+    settings = {
+      "*" = {
+        AddKeysToAgent = "yes";
+        Compression = false;
+        ServerAliveInterval = 0;
+        ServerAliveCountMax = 3;
+        HashKnownHosts = false;
+        UserKnownHostsFile = "~/.ssh/known_hosts";
+        ControlMaster = "no";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "no";
+      };
+      "github.com" = {
+        HostName = "ssh.github.com";
+        Port = 443;
+        User = "git";
+        IdentitiesOnly = true;
+        IdentityFile = "${config.home.homeDirectory}/.ssh/github-key";
+      };
+      "192.168.*" = {
+        IdentityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
+        IdentitiesOnly = true;
+      };
+    };
+  };
+}
