e5f2e937a36ad55ab515db65ef9c3f2d2d3e6544
zeno <65364211+telometto@users.noreply.github.com>
2026-06-28 00:29:22 +0200

    🧹 chore(roles): add mutual exclusivity assertion for desktop and server role toggles

--- modules/core/roles.nix
+++ modules/core/roles.nix
@@ -1,7 +1,14 @@
-{ lib, ... }:
+{ lib, config, ... }:
 {
   options.sys.role = {
     desktop.enable = lib.mkEnableOption "Desktop role toggle";
     server.enable = lib.mkEnableOption "Server role toggle";
   };
+
+  config.assertions = [
+    {
+      assertion = !(config.sys.role.desktop.enable && config.sys.role.server.enable);
+      message = "sys.role.desktop.enable and sys.role.server.enable are mutually exclusive";
+    }
+  ];
 }
