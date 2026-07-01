df73379adb68c0f8774667454e31d6ccf6a1a64a
zeno <65364211+telometto@users.noreply.github.com>
2026-07-01 11:28:59 +0200

    chore(gaming): add extraCompatPackages to Steam

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -56,6 +56,7 @@ in
           steamtinkerlaunch
           protonplus
         ];
+        extraCompatPackages = with pkgs; [ proton-ge-bin ];
         fontPackages = FONTS;
       };
       gamemode.enable = lib.mkDefault true;
