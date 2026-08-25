20f0e4e7ebcf6f44de25814dde72c6b5019dc0f0
Zeno <65364211+telometto@users.noreply.github.com>
2026-08-25 15:19:33 +0200

    test(user-accounts): add user account tests for UID and SOPS paths

---
+++ tests/user-accounts.nix
@@ -0,0 +1,15 @@
+{ kaizer, pkgs, snowfall, VARS }:
+let
+  snowfallConfig = snowfall.config;
+  kaizerConfig = kaizer.config;
+  zenoUsername = VARS.users.zeno.user;
+  lukeUsername = VARS.users.luke.user;
+  zenoHome = snowfallConfig.home-manager.users.${zenoUsername};
+in
+assert snowfallConfig.users.users.${zenoUsername}.uid == 1000;
+assert kaizerConfig.users.users.${lukeUsername}.uid == null;
+assert zenoHome.sops.defaultSymlinkPath == "%r/secrets";
+assert zenoHome.sops.defaultSecretsMountPoint == "%r/secrets.d";
+pkgs.runCommand "user-account-tests" { } ''
+  touch "$out"
+''
